bbc5e81b7ad9a1b86165444d122ec759502da104
Michelange MBETIBANGA <michelangembetibanga@yahoo.fr>
2024-05-14 16:20:55 +0200

    Start to work on Arrays

---
+++ arrays.js
@@ -0,0 +1,17 @@
+
+// Adding Elements
+
+const numbers = [3, 4];
+
+// End
+numbers.push(5, 6);
+
+// Begenning
+numbers.unshift(1, 2);
+
+// Middle
+numbers.splice(2, 0, 'a', 'b');
+
+console.log(numbers);
+
+// Finding Elements (Primitives)
