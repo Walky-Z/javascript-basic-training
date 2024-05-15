2b5a0c618a6240c3cc9ae7186cfd32dc1800a864
Michelange MBETIBANGA <michelangembetibanga@yahoo.fr>
2024-05-15 20:11:57 +0200

    achievement

--- arrays.js
+++ arrays.js
@@ -1,7 +1,7 @@
 
-// Adding Elements
+// 1 - Adding Elements
 
-const numbers = [3, 4];
+const numbers = [3, 4, 1];
 
 // End
 numbers.push(5, 6);
@@ -14,4 +14,23 @@ numbers.splice(2, 0, 'a', 'b');
 
 console.log(numbers);
 
-// Finding Elements (Primitives)
+// 2 - Finding Elements (Primitives)
+
+console.log(numbers.indexOf(1));
+console.log(numbers.lastIndexOf(1));
+
+console.log(numbers.indexOf(1) !== -1);
+// Equivalent
+console.log(numbers.includes(1));
+
+// Search begin with a specific index
+console.log(numbers.indexOf(1, 2)); // => 6
+
+// 3 - Finding Elements (Reference Types)
+
+const courses = [
+  {id: 1, name: 'a'},
+  {id: 2, name: 'b'},
+];
+
+courses.find()
