7336998ef81da014142e9c6d4beaf71b674c179e
Michelange MBETIBANGA <michelangembetibanga@yahoo.fr>
2024-05-15 20:40:55 +0200

    Working on arrays

--- arrays.js
+++ arrays.js
@@ -33,4 +33,49 @@ const courses = [
   {id: 2, name: 'b'},
 ];
 
-courses.find()
+// return the element
+const course = courses.find(function(course) {
+  return course.name === 'a';
+});
+
+// return the index
+const course2 = courses.findIndex(function(course) {
+  return course.name === 'a';
+});
+
+console.log(course, course2);
+
+// 4 - Arrow Functions
+const course3 = courses.find(course => course.name === 'a');
+console.log(course3);
+
+// 5 - Removing Elements
+
+const nombres = [1, 2, 3 ,4];
+
+// End
+//const last = nombres.pop(); // remove and return the last element
+
+// Begenning
+//const first = nombres.shift(); // remove and return the first element
+
+// Middle
+nombres.splice(2, 1); // remove the specify element indexed
+// The second number indicate how many element to be removed
+console.log(nombres);
+
+// 5 - Emptying an Array
+let nb = [1, 2, 3 ,4];
+
+// Solution 1 - reassign (not erase the memory)
+// nb = [];
+
+// Solution 2 - Truncate (recommended)
+//nb.length = 0;
+
+// Solution 3 - Splice method
+// nb.splice(0, nb.length);
+
+// Solution 4 - Pop method (not recommended for big arrays)
+// while(nb.length > O)
+//   nb.pop();
