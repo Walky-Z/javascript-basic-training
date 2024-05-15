c3b0c1f2b2d5f730d27783605baa4bf96e4d20c4
Michelange MBETIBANGA <michelangembetibanga@yahoo.fr>
2024-05-15 21:58:28 +0200

    working on arrays

--- arrays.js
+++ arrays.js
@@ -79,3 +79,82 @@ let nb = [1, 2, 3 ,4];
 // Solution 4 - Pop method (not recommended for big arrays)
 // while(nb.length > O)
 //   nb.pop();
+
+// 6 - Combining and Slicing Arrays
+
+const firstarray = [1, 2, 3];
+const secondarray = [4, 5, 6];
+
+// const combined = firstarray.concat(secondarray);
+
+// const slice = combined.slice(2, 4);
+
+const firstobj = [{id: 1}]
+// if element are primitive (number), they are copied by value
+// if they are reference (object) they are copied by reference
+
+// 7 - Spread Operator
+
+const combined = [...firstarray, 'a', ...secondarray, 'b'];
+console.log(combined); // [1, 2, 3, 'a', 4 ,5 , 6, 'b']
+
+const copy =[...combined];
+console.log(copy);
+
+// 8 - Iterating an Array
+
+const nombre = [1, 2, 3];
+
+// Solution 1
+for (let number of nombre)
+  console.log(number);
+// Solution 2
+nombre.forEach(function (number) {
+  console.log(number);
+})
+// solution 3
+nombre.forEach(number => console.log(number));
+
+nombre.forEach((number, index) => console.log(index, number));
+
+// 9 - Joining Arrays
+
+const joined = nombre.join(',');
+console.log(joined); // 1, 2, 3
+
+const message = 'This is my first message';
+const parts = message.split(' ');
+console.log(parts); // ["This", "is", "my", "first", "message"]
+
+const combined2 = parts.join('_');
+console.log(combined2);
+
+// 10 - Sorting Arrays
+
+// For numbers
+const exemple = [2, 3, 1];
+exemple.sort();
+console.log(exemple)
+
+// For objects
+const cours = [
+  {id: 1, name: 'Node.js'},
+  {id: 2, name: 'JavaScript'}
+];
+
+cours.sort(function(a, b) {
+  // a < b => -1
+  // a > b => 1
+  // a == b => 0
+  const nameA = a.name.toLowerCase();
+  const nameB = b.name.toLowerCase();
+
+  if (nameA < nameB) return -1;
+  if (nameA > nameB) return 1;
+  return 0;
+
+})
+
+console.log(cours);
+
+// 11 - Testing the Elements of an Array
