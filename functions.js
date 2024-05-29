9e4ed2684ada4d285d7cad3ef02e62cb20c368b5
Michelange MBETIBANGA <michelangembetibanga@yahoo.fr>
2024-05-29 20:33:08 +0200

    Exercice 1

--- functions.js
+++ functions.js
@@ -223,3 +223,13 @@ video5.showTags();
 // Recap
 // Using self
 // Using bind() method
+
+// Exercice 1
+
+function sum(...args) {
+  if (args.length === 1 && Array.isArray(args[0]))
+    args = [...args[0]];
+  return args.reduce((a, b) => a + b);
+}
+
+console.log(sum(1, 2, 3, 4));
